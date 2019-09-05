ab660f9f6eea4474299eab73c8adc5c62cb5e598
pscott <pscott@student.42.fr>
2019-09-05 20:56:08 +0200

    Rename Cluster to Group; Add private helper method for easier conversion; Use match instead of ifs

--- src/converter.rs
+++ src/converter.rs
@@ -1,7 +1,5 @@
-static ZERO_TO_TEN: [&str; 10] = [
+static ZERO_TO_TWENTY: [&str; 20] = [
     "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
-];
-static TEN_TO_TWENTY: [&str; 10] = [
     "ten",
     "eleven",
     "twelve",
@@ -18,18 +16,18 @@ static TENS: [&str; 10] = [
 ];
 static CATEGORIES: [&str; 7] = ["", "thousand", "million", "billion", "trillion", "quadrillion", "quintillion"];
 
-pub struct Cluster {
+pub struct Group {
     hundreds: u8,
     tens: u8,
     units: u8,
     category: &'static str,
 }
 
-impl Cluster {
+impl Group {
     pub fn new(num: u16, category: &'static str) -> Self {
         assert!(num < 1000);
 
-        Cluster {
+        Group {
             hundreds: (num / 100) as u8,
             tens: (num / 10 % 10) as u8,
             units: (num % 10) as u8,
@@ -37,33 +35,36 @@ impl Cluster {
         }
     }
 
-    pub fn humanized(&self) -> String {
-        let first_digit = match self.hundreds {
+    fn first_digit(&self) -> String {
+        match self.hundreds {
             0 => String::new(),
-            _ => format!("{} hundred", ZERO_TO_TEN[self.hundreds as usize]),
-        };
+            _ => format!("{} hundred", ZERO_TO_TWENTY[self.hundreds as usize]),
+        }
+    }
 
-        let under_twenty = self.tens * 10 + self.units;
-
-        let last_digits = if self.units == 0 {
-            TENS[self.tens as usize].to_owned()
-        } else if under_twenty < 10 {
-            ZERO_TO_TEN[under_twenty as usize].to_owned()
-        } else if under_twenty < 20 {
-            TEN_TO_TWENTY[(under_twenty - 10) as usize].to_owned()
-        } else {
-            format!(
+    fn two_last_digits(&self) -> String {
+        match (self.tens, self.units) {
+            (0, _)  =>  ZERO_TO_TWENTY[self.units as usize].to_owned(),
+            (1, _)  =>  ZERO_TO_TWENTY[(self.tens * 10 + self.units) as usize].to_owned(),
+            (_, 0)  =>  TENS[self.tens as usize].to_owned(),
+            (_, _)  =>  format!(
                 "{} {}",
-                TENS[self.tens as usize], ZERO_TO_TEN[self.units as usize]
-            )
+                TENS[self.tens as usize], ZERO_TO_TWENTY[self.units as usize]
+            ),
+        }
+    }
+
+    pub fn humanized(&self) -> String {
+        let first_digit = self.first_digit();
+        let two_last_digits = self.two_last_digits();
+
+        let english_number = match (first_digit.is_empty(), two_last_digits.is_empty()) {
+            (true, true) => String::new(),
+            (true, false) => two_last_digits,
+            (false, true) => first_digit,
+            (false, false) => format!("{} {}", first_digit, two_last_digits),
         };
 
-        let english_number = match (first_digit.is_empty(), last_digits.is_empty()) {
-            (true, true) => String::new(),
-            (true, false) => last_digits,
-            (false, true) => first_digit,
-            (false, false) => format!("{} {}", first_digit, last_digits),
-        };
         if self.category.is_empty() || english_number.is_empty() {
             english_number
         } else {
@@ -77,8 +78,8 @@ pub fn number_to_english(mut num: u64) -> String {
     let mut category = 0;
     while num != 0 {
         let slice = (num % 1000) as u16;
-        let cluster = Cluster::new(slice, CATEGORIES[category]);
-        vec.push(cluster.humanized());
+        let group = Group::new(slice, CATEGORIES[category]);
+        vec.push(group.humanized());
         category += 1;
         num /= 1000;
     }
